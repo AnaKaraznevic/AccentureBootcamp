acdd5f2786116200e6aa4ed41af033b2c0c5a36d
Ana Karaznevic <andrulenaite@gmail.com>
2022-08-18 13:58:31 +0300

    Second commit

--- MultiplicationTable.java
+++ MultiplicationTable.java
@@ -1,3 +1,18 @@
+/*
+Write a program which creates two dimensional
+// array and stores a multiplication table (from 1 - 10) in it.
+    Example of output:
+        1 * 1 = 1
+        1 * 2 = 2
+        1 * 3 = 3
+        ....
+        10 * 9 = 90
+        10 * 10 = 100
+    Hint:
+       1) don't forget that you have to save the values in the array and output them from the array.
+       2) Arrays should be the size of [10][10]
+ */
+
 public class MultiplicationTable {
 
     public static void main (String[] args){

a62659392e39a614085815ac3db6622286889a15
Ana Karaznevic <andrulenaite@gmail.com>
2022-08-18 14:36:05 +0300

    First commit

---
+++ ArrayCopyIterations.java
@@ -0,0 +1,30 @@
+/*
+Declare and initialize an array with four arbitrary whole numbers.
+Write a Java program to copy this array by iterating it.
+ */
+
+import java.util.Arrays;
+
+public class ArrayCopyIterations {
+
+    public static void main (String args[]){
+        int[] arrayInitial = {1,6,39,67};
+        int[] arrayCopy;
+
+        arrayCopy = copyArray(arrayInitial);
+
+        System.out.println("Initial :" + Arrays.toString(arrayInitial));
+        System.out.println("Copy :" + Arrays.toString(arrayCopy));
+    }
+
+    public static int[] copyArray (int[] arrayFrom){
+        int length;
+        length = arrayFrom.length;
+        int [] arrayTo = new int[length];
+
+        for (int i = 0; i < length ; i++){
+            arrayTo[i] = arrayFrom[i];
+        }
+        return arrayTo;
+    }
+}
